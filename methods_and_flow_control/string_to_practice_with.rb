b5c4ecc011834ca71b3871fccbc69ecc44470fe2
Judith Barrett <judith.barrett@gmail.com>
2014-10-07 22:27:47 -0600

    Modified string_to_practice_with.rb

--- methods_and_flow_control/string_to_practice_with.rb
+++ methods_and_flow_control/string_to_practice_with.rb
@@ -4,4 +4,6 @@ puts practice_string.upcase
 puts practice_string.capitalize.inspect
 puts "The string '#{practice_string}' has #{practice_string.length} characters"
 puts practice_string.reverse
-puts "#{practice_string.slice!("practice ") * 3}!"
+# puts "#{practice_string.slice("practice ") * 3}!"
+# This outputs "practice practice practice !", so....
+puts "#{practice_string.slice("practice ") * 2 << practice_string.slice("practice") * 1}!"
