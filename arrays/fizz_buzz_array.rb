55dd91bb3eedabac8a3351eadecc23db440474de
Judith Barrett <judith.barrett@gmail.com>
2014-10-15 15:43:14 -0600

    added fizz_buzz_array file

---
+++ arrays/fizz_buzz_array.rb
@@ -0,0 +1,18 @@
+#!usr/bin/env ruby
+
+(1..100).each do |numbers|
+  fizz = (numbers % 3 == 0)
+  buzz = (numbers % 5 == 0)
+
+    if fizz && buzz
+      puts "Fizzbuzz"
+    end
+
+    if fizz
+     puts "Fizz"
+    elsif buzz
+     puts "Buzz"
+    else
+    puts numbers
+    end
+  end
