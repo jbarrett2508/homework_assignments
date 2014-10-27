263c3b2bd908c886dd47802ea6b605f1a75dfc6c
Judith Barrett <judith.barrett@gmail.com>
2014-10-27 14:00:03 -0600

    Fixed fizz_buzz.rb file and pushed to methods_and_flow_control repo

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -2,18 +2,14 @@
 
 (1..100).each do |number|
 
-  if number % 3 == 0
-    if number % 5 == 0
+  if ((number % 3) && (number % 5 == 0))
       puts "Fizzbuzz"
+
+  elsif number % 3 == 0
+    puts "Fizz"
+    elsif number % 5 == 0
+      puts "Buzz"
+    else
+      puts number.to_s
     end
   end
-
-  if number % 3 == 0
-    puts "Fizz"
-  elsif number % 5 == 0
-    puts "Buzz"
-  else
-    puts number.to_s
-  end
-end
-
